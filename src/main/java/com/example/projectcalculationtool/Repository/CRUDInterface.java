f76e47e3f3e8db8dbb764c007298af569c6e319f
AdamHoppe98 <adamhoppe98@gmail.com>
2025-11-13 12:49:00 +0100

    sql table create

--- src/main/java/com/example/projectcalculationtool/Repository/CRUDInterface.java
+++ src/main/java/com/example/projectcalculationtool/Repository/CRUDInterface.java
@@ -1,4 +1,10 @@
 package com.example.projectcalculationtool.Repository;
 
+import java.util.List;
+
 public interface CRUDInterface {
+    void create(Object o);
+    List<Object> getAll();
+    void update(Object o);
+    void delete(int id);
 }
