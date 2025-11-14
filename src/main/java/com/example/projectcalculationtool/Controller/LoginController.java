2282c48f57cf166698d270869aff9c76eaccdbc0
Niinyah <petermarbell@gmail.com>
2025-11-14 12:32:41 +0100

    isloggedin method

--- src/main/java/com/example/projectcalculationtool/Controller/LoginController.java
+++ src/main/java/com/example/projectcalculationtool/Controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.example.projectcalculationtool.Controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginController {
+
+    private boolean isLoggedIn(HttpSession session) {
+        session.getAttribute("Member");
+        return session.getAttribute("Member") != null;
+    }
 
 }
