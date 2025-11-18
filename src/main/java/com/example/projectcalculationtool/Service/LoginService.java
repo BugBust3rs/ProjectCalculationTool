f4d3b0b47d3e97148560d9a4e60dac80d059188b
Niinyah <petermarbell@gmail.com>
2025-11-18 13:33:42 +0100

    Login

--- src/main/java/com/example/projectcalculationtool/Service/LoginService.java
+++ src/main/java/com/example/projectcalculationtool/Service/LoginService.java
@@ -1,4 +1,16 @@
 package com.example.projectcalculationtool.Service;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
+
+    public boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("memberId") != null;
+    }
+
+    public void InvalidateSession(HttpSession session){
+        session.invalidate();
+    }
 }
