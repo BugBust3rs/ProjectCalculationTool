fcc3ee4af149d24bb10b1f64ef15fc08f1894ab7
Silverspit <philipraissnia-2@hotmail.com>
2025-11-20 11:05:44 +0100

    .

--- src/main/java/com/example/projectcalculationtool/Controller/ProjectController.java
+++ src/main/java/com/example/projectcalculationtool/Controller/ProjectController.java
@@ -3,6 +3,7 @@ package com.example.projectcalculationtool.Controller;
 import com.example.projectcalculationtool.Model.Member;
 import com.example.projectcalculationtool.Model.Project;
 import com.example.projectcalculationtool.Service.LoginService;
+import com.example.projectcalculationtool.Service.MemberService;
 import com.example.projectcalculationtool.Service.ProjectService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -17,9 +18,12 @@ public class ProjectController {
 
     private final ProjectService projectService;
     private final LoginService loginService;
-    public ProjectController(ProjectService projectService, LoginService loginService) {
+    private final MemberService memberService;
+
+    public ProjectController(ProjectService projectService, LoginService loginService, MemberService memberService) {
         this.projectService = projectService;
         this.loginService = loginService;
+        this.memberService = memberService;
     }
 
     @GetMapping("/dashboard")
@@ -38,7 +42,7 @@ public class ProjectController {
     @PostMapping("/deleteProject/{projectId}")
     public String deleteProject(@PathVariable int projectId, HttpSession session) {
         int memberId = (int) session.getAttribute("memberId");
-        if (!loginService.isLoggedIn(session)|| !projectService.memberHasProject(projectId, memberId)) {
+        if (!loginService.isLoggedIn(session) || !projectService.memberHasProject(projectId, memberId)) {
             return "redirect:/login";
         }
         projectService.deleteProject(projectId);
@@ -63,6 +67,20 @@ public class ProjectController {
         int memberId = (int) session.getAttribute("memberId");
         projectService.saveProject(project, memberId);
         return "redirect:/dashboard" + project.getProjectId();
+
+    }
+
+    @PostMapping("/taskOverview{email}")
+    public String inviteMemberToProject(@PathVariable String email, HttpSession session) {
+
+        //        if (!isLoggedIn(session)) {
+//            return "redirect:/login";
+//        }
+
+        int memberId = (int) session.getAttribute("memberId");
+        Member member = memberService.addMemberToProject(email);
+        List<Member> members = projectService.getMembersOnProject(email);
+        me
 
     }
 
